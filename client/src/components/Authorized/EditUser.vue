751cce0f0b67c73884b2f803bc8efa501c16a27e
Nikit <amigo7772015@mail.ru>
2025-05-13 18:44:29 +0300

    Исправлена адаптация окон редактирования

--- client/src/components/Authorized/EditUser.vue
+++ client/src/components/Authorized/EditUser.vue
@@ -113,18 +113,4 @@ export default {
 @import "@/assets/css/edits.css";
 @import "@/assets/css/media/edits_media.css";
 
-.userSettings {
-    position: fixed;
-    z-index: 10;
-
-    width: 100vw;
-    height: 100vh;
-    left: 0;
-    top: 0;
-    background-color: var(--color_background-5_grayTransparent);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    backdrop-filter: blur(3px);
-}
 </style>
